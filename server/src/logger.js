3608e4c5a55401ef9713f678db90a4adbf12fb15
atrancandoris <atran@candoris.com>
2020-06-04 10:32:26 -0400

    Refactor & add docker tools

--- server/src/logger.js
+++ server/src/logger.js
@@ -1,36 +1,34 @@
-const path = require('path');
-const {
-  createLogger,
-  format,
-  transports,
-} = require('winston');
-const config = require('../../../config');
+const path = require("path");
+const { createLogger, format, transports } = require("winston");
+const config = require("../../config");
 
-const logFormat = format.printf(({ level, message }) => `${level}: ${JSON.stringify(message, null, 2)}`);
+const logFormat = format.printf(
+  ({ level, message }) => `${level}: ${JSON.stringify(message, null, 2)}`
+);
 
 const logger = createLogger({
-  format: format.combine(
-    format.colorize(),
-    format.simple(),
-    logFormat,
-  ),
+  format: format.combine(format.colorize(), format.simple(), logFormat),
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.level = 'debug';
-  logger.add(
-    new transports.Console(),
-  );
+if (process.env.NODE_ENV !== "production") {
+  logger.level = "debug";
+  logger.add(new transports.Console());
 } else {
-  logger.level = 'info';
+  logger.level = "info";
   logger.add(
     new transports.File({
-      filename: path.join(process.env.LOG_PATH, `${config.projectName}_error.log`),
-      level: 'error',
+      filename: path.join(
+        process.env.LOG_PATH,
+        `${config.projectName}_error.log`
+      ),
+      level: "error",
     }),
     new transports.File({
-      filename: path.join(process.env.LOG_PATH, `${config.projectName}_combined.log`),
-    }),
+      filename: path.join(
+        process.env.LOG_PATH,
+        `${config.projectName}_combined.log`
+      ),
+    })
   );
 }
 
--- server/src/sequelize.js
+++ server/src/sequelize.js
@@ -1,8 +1,8 @@
-const Sequelize = require('sequelize');
-const readdirSync = require('fs').readdirSync;
-const tablesPath = require('path').join(__dirname, '../models/tables');
-const config = require('../../../config');
-const logger = require('./logger');
+const Sequelize = require("sequelize");
+const readdirSync = require("fs").readdirSync;
+const tablesPath = require("path").join(__dirname, "../models/tables");
+const config = require("../../config");
+const logger = require("./logger");
 
 const sequelize = new Sequelize(config.db);
 
@@ -10,10 +10,13 @@ const models = {};
 
 // Load Tables
 readdirSync(tablesPath).forEach((file) => {
-  const modelName = file.replace('.js', '');
+  const modelName = file.replace(".js", "");
   logger.info(`Initializing model for table ${modelName}`);
 
-  models[modelName] = require(`${tablesPath}/${file}`).init(sequelize, Sequelize);
+  models[modelName] = require(`${tablesPath}/${file}`).init(
+    sequelize,
+    Sequelize
+  );
 });
 
 // Associate Models
